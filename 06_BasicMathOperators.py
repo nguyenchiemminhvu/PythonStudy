004d03db5c4611658eaed49b0099a947764723b8
nguyenchiemminhvu <nguyenchiemminhvu@gmail.com>
2023-12-07 23:09:06 +0700

    update

---
+++ 06_BasicMathOperators.py
@@ -0,0 +1,27 @@
+print("10 + 4 =", 10 + 4)
+print("4 - 23 =", 5 - 23)
+print("18 * 3600000.0 =", 18 * 3600000.0)
+print("3 / 5 =", 3 / 5)
+print("3 // 5 =", 3 // 5) # integer result
+print("3 module 5 =", 3 % 5)
+print("pow(2, 4) =", 2 ** 4) # pow(2, 4)
+
+x = 5
+x = x + 3
+x += 3
+
+y = 3
+y *= 3
+
+z = 10
+z -= 5
+
+print("x + y * z =", x + y * z)
+
+print("10 + 3 * 2 ** 2 = ", 10 + 3 * 2 ** 2) # <=> pow(2, 2) * 3 + 10
+
+print("(10 + 3) * 2 ** 2 =", (10 + 3) * 2 ** 2) # <=> pow(2, 2) * 13 => parentheses make priority
+
+print("round(3.9) =", round(3.9))
+print("round(3.4) =", round(3.4))
+print("round(3.5) =", round(3.5))
