c5555dc3acf612a7e8575e5dc2a8216be5b242a9
nguyenchiemminhvu <nguyenchiemminhvu@gmail.com>
2023-12-10 23:36:26 +0700

    update

--- 12_Dictionary.py
+++ 12_Dictionary.py
@@ -19,12 +19,21 @@ methods = [method for method in dir(members[0]) if not method.startswith("__")]
 print("All the methods that a Dictionary supports:")
 print(methods)
 
-print("List of members:")
+print("List of members before updating:")
 for member in members:
     print(member)
 
 for member in members:
     if (not member.get("phone") is None):
         print("member {} has phone number".format(member.get("name")))
+    else:
+        member["phone"] = "N/A"
+    
     if (not member.get("bank_id") is None):
         print("member {} has bank account".format(member.get("name")))
+    else:
+        member["bank_id"] = "N/A"
+
+print("List of members after updating:")
+for member in members:
+    print(member)
