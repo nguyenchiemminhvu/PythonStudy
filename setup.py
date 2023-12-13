793c6873893aeaa1371b6543cd34758a612df57d
nguyenchiemminhvu <nguyenchiemminhvu@gmail.com>
2023-12-13 16:19:32 +0700

    update

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='PythonStudy',
+    version='1.0.0',
+    description='I used this module as a reference to what I have learned',
+    author='NCMV',
+    packages=['.'],
+)
