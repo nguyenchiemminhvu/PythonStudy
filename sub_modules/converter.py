53da8446e2f8631d0f9dbd266d1e45fb3e0a9b8f
nguyenchiemminhvu <nguyenchiemminhvu@gmail.com>
2023-12-14 23:34:28 +0700

    update

--- sub_modules/converter.py
+++ sub_modules/converter.py
@@ -16,3 +16,9 @@ def collection_to_string(collection):
     else:
         output += str(collection)
     return output
+
+if __name__ == "__main__":
+    import sys
+    args = sys.argv[1:]
+    output = collection_to_string(args)
+    print(output)
