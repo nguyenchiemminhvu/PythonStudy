afa17359fcfac83f610ff11c4c5d18146e877090
nguyenchiemminhvu <nguyenchiemminhvu@gmail.com>
2023-12-14 23:55:09 +0700

    update

--- 17_Modules.py
+++ 17_Modules.py
@@ -1,5 +1,9 @@
-from sub_modules import converter
+import sys
+before_import = set(sys.modules.items())
+import sub_modules.converter
+after_import = set(sys.modules.items())
+print("Which module is just imported? ->", after_import - before_import)
 
 nums = [1,2,3,4,5]
-output = converter.collection_to_string(nums)
+output = sub_modules.converter.collection_to_string(nums)
 print(output)
