9eafbc0f0c4318c3c25f04af4688c7c24adafac4
nguyenchiemminhvu <nguyenchiemminhvu@gmail.com>
2023-12-17 11:43:44 +0700

    update

--- 18_BuiltInModules.py
+++ 18_BuiltInModules.py
@@ -1,5 +1,25 @@
 import os
-import shutil
+import sys
+import urllib.request
+import zlib
+import timeit
 
-print(dir(os))
 os.system("git status")
+sys.stdout.write("write text to stdout file stream\n")
+sys.stderr.write("write text to stderr file stream\n")
+
+s = b"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+print(s, len(s))
+s = zlib.compress(s)
+print(s, len(s))
+s = zlib.decompress(s)
+print(s, len(s))
+
+request = """
+import urllib.request
+with urllib.request.urlopen("https://www.google.com") as response:
+    for line in response:
+        print(line)
+"""
+time_cost = timeit.timeit(request, number=1)
+print(time_cost, "seconds")
