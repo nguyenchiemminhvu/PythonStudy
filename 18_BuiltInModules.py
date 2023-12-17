98b83c7e90d416c52415ab6b609870a005a72a80
nguyenchiemminhvu <nguyenchiemminhvu@gmail.com>
2023-12-17 12:12:56 +0700

    update

--- 18_BuiltInModules.py
+++ 18_BuiltInModules.py
@@ -1,12 +1,23 @@
 import os
 import sys
+import logging
 import urllib.request
 import zlib
 import timeit
+import textwrap
+import array
 
 os.system("git status")
 sys.stdout.write("write text to stdout file stream\n")
 sys.stderr.write("write text to stderr file stream\n")
+
+
+logging.basicConfig(level=logging.INFO)
+logging.debug('This is a debug message')
+logging.info('This is an info message')
+logging.warning('This is a warning message')
+logging.error('This is an error message')
+logging.critical('This is a critical message')
 
 s = b"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
 print(s, len(s))
@@ -14,6 +25,7 @@ s = zlib.compress(s)
 print(s, len(s))
 s = zlib.decompress(s)
 print(s, len(s))
+print(textwrap.fill(str(s), width=10))
 
 request = """
 import urllib.request
