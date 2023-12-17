cea95e65b6edc3c548729ef10fcf48eadfef7962
nguyenchiemminhvu <nguyenchiemminhvu@gmail.com>
2023-12-17 20:03:26 +0700

    update

--- 22_Multithreading.py
+++ 22_Multithreading.py
@@ -2,6 +2,7 @@ import logging
 import threading
 
 def thread_func():
+    logging.info("thread id " + str(threading.current_thread().native_id) + " is executing")
     for i in range(0, 10):
         print(i)
 
