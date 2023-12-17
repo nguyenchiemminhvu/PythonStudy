13b7fc78d1b6238877d6410def7e17f771401f18
nguyenchiemminhvu <nguyenchiemminhvu@gmail.com>
2023-12-17 21:37:07 +0700

    update

--- 22_Multithreading.py
+++ 22_Multithreading.py
@@ -1,19 +1,24 @@
 import logging
 import threading
 
-def thread_func():
+
+def thread_func(param):
     logging.info("thread id " + str(threading.current_thread().native_id) + " is executing")
     for i in range(0, 10):
         print(i)
+        param.append(i)
+
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
     logging.info("before creating thread")
-    sub_thread = threading.Thread(target=thread_func, name="sub_thread")
+    return_value = []
+    sub_thread = threading.Thread(target=thread_func, name="sub_thread", args=(return_value,))
     logging.info("before running thread")
     sub_thread.start()
     logging.info("waiting for sub_thread done")
     sub_thread.join()
+    logging.info("return value: {}".format(return_value))
     logging.info("sub_thread done")
     logging.info("all done")
     
