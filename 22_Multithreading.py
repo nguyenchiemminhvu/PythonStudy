5721a9d24b5b3f1b2e36ed7be36835531fd9664c
nguyenchiemminhvu <nguyenchiemminhvu@gmail.com>
2023-12-17 21:45:35 +0700

    update

--- 22_Multithreading.py
+++ 22_Multithreading.py
@@ -1,24 +1,51 @@
-import logging
+from collections.abc import Callable, Iterable, Mapping
+from typing import *
+import time
 import threading
+from typing import Any
+
+
+class SampleThread(threading.Thread):
+    def __init__(self, group: None = None, target: Callable[..., object] | None = None, name: str | None = None, args: Iterable[Any] = ..., kwargs: Mapping[str, Any] | None = None, *, daemon: bool | None = None) -> None:
+        super().__init__(group, target, name, args, kwargs, daemon=daemon)
+    
+    def run(self) -> None:
+        self.arr = []
+        for i in range(0, 10):
+            time.sleep(0.1)
+            self.arr.append(i)
+        return super().run()
+
+
+class DaemonService(threading.Thread):
+    def __init__(self, group: None = None, target: Callable[..., object] | None = None, name: str | None = None, args: Iterable[Any] = ..., kwargs: Mapping[str, Any] | None = None, *, daemon: bool | None = None) -> None:
+        super().__init__(group, target, name, args, kwargs, daemon=daemon)
+    
+    def run(self) -> None:
+        while (True):
+            time.sleep(0.1)
+            print("Checking system status")
+        return super().run()
 
 
 def thread_func(param):
-    logging.info("thread id " + str(threading.current_thread().native_id) + " is executing")
     for i in range(0, 10):
-        print(i)
+        time.sleep(0.1)
         param.append(i)
 
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO)
-    logging.info("before creating thread")
+    
+    daemon_thread = DaemonService(name="daemon_service", daemon=True)
+    daemon_thread.start()
+    
     return_value = []
     sub_thread = threading.Thread(target=thread_func, name="sub_thread", args=(return_value,))
-    logging.info("before running thread")
     sub_thread.start()
-    logging.info("waiting for sub_thread done")
     sub_thread.join()
-    logging.info("return value: {}".format(return_value))
-    logging.info("sub_thread done")
-    logging.info("all done")
+    print("return value from thread {}: {}".format(sub_thread.name, return_value))
     
+    another_sub_thread = SampleThread()
+    another_sub_thread.start()
+    another_sub_thread.join()
+    print("return value from thread {}: {}".format(another_sub_thread.name, another_sub_thread.arr))
