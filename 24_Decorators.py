fc4b92254cf6368883e988a3d591d17b3ac5f386
vu3.nguyen <vu3.nguyen@lge.com>
2023-12-21 18:03:15 +0900

    update

--- 24_Decorators.py
+++ 24_Decorators.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 def sample_decorator(func):
     def wrapper():
         print(f"before {func.__name__}")
@@ -6,9 +8,31 @@ def sample_decorator(func):
     
     return wrapper
 
+def decor_silent_night(func):
+    def wrapper():
+        if (datetime.now().hour < 7 and datetime.now().hour < 22):
+            print("Keep silent during the night")
+        else:
+            print("Do it in day time")
+            func()
+    return wrapper
+
 def sample_func():
     print("sample function is called")
+
+@sample_decorator
+def shorten_decor():
+    print("sample function is called")
+
+@decor_silent_night
+def say_out_loud():
+    print("Say out loud!!!")
 
 if __name__ == "__main__":
     sample_func = sample_decorator(sample_func)
     sample_func()
+    
+    shorten_decor()
+    
+    say_out_loud()
+    
