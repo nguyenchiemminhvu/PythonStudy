7cde0aba2f55e565ee04c0f4f568153b7b5cab2e
nguyenchiemminhvu <nguyenchiemminhvu@gmail.com>
2023-12-21 22:59:43 +0700

    update

---
+++ 25_Numpy.py
@@ -0,0 +1,12 @@
+import numpy
+
+if __name__ == "__main__":
+    print(numpy.zeros(3))
+    print(numpy.ones(3))
+    print(numpy.empty(3))
+    print(numpy.arange(0, 10, 1))
+    print(numpy.arange(0, 10, 2))
+    print(numpy.arange(0, 10, 3))
+    print(numpy.linspace(0, 10, 1))
+    print(numpy.linspace(0, 10, 2))
+    print(numpy.linspace(0, 10, 3))
